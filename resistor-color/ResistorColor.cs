3d2fe67abc14ae654ba9a336a7f2c60159399066
Nikita Brodel <69465596+nbrodel@users.noreply.github.com>
2021-03-08 08:31:37 +0600

    add resistor-color
    resistor color track

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        return Array.IndexOf(_colors, color);
+    }
+
+    private static readonly string[] _colors = new { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+}
